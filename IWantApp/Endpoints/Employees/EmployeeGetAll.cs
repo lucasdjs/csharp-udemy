c1894b6921163f4d3fd2440f613c5e705648c721
Analista Testes Junior de Jesus Silva <lucas.jesus@ndd.tech>
2023-04-30 02:29:35 -0300

    Criando Serviços customizados

--- IWantApp/Endpoints/Employees/EmployeeGetAll.cs
+++ IWantApp/Endpoints/Employees/EmployeeGetAll.cs
@@ -1,7 +1,4 @@
-﻿using Dapper;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Data.SqlClient;
-using System.Security.Claims;
+﻿using IWantApp.Infra.Data;
 
 namespace IWantApp.Endpoints.Employees;
 
@@ -11,23 +8,13 @@ public class EmployeeGetAll
     public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
     public static Delegate Handle => Action;
 
-    public static IResult Action(int? page, int? rows, IConfiguration configuration)
+    public static IResult Action(int? page, int? rows, QueryAllUsersWithClaimName query)
     {
-        if(page == null || rows == null || rows> 10)
+        if (page == null || rows == null || rows > 10)
         {
             return Results.BadRequest("Deve ser informado a paginação corretamente");
         }
 
-        var db = new SqlConnection(configuration["Database:IWantDb"]);
-        var query = @"select Email, ClaimValue as Name
-            from AspNetUsers u inner join 
-            AspNetUserClaims c on u.id = c.UserId and claimType = 'Name'
-            order by Name
-            OFFSET (@page - 1 )* @rows ROWS FETCH NEXT @rows ROWS ONLY";
-
-        var employees = db.Query<EmployeeResponse>(query, new {page, rows });
-
-
-        return Results.Ok(employees);
+        return Results.Ok(query.Execute(page.Value, rows.Value));
     }
 }
---
+++ IWantApp/Infra/Data/QueryAllUsersWithClaimName.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using IWantApp.Endpoints.Employees;
+using Microsoft.Data.SqlClient;
+
+namespace IWantApp.Infra.Data;
+
+public class QueryAllUsersWithClaimName
+{
+    private readonly IConfiguration configuration;
+
+    public QueryAllUsersWithClaimName(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public IEnumerable<EmployeeResponse> Execute(int page, int rows) {
+
+        var db = new SqlConnection(configuration["Database:IWantDb"]);
+        var query = @"select Email, ClaimValue as Name
+            from AspNetUsers u inner join 
+            AspNetUserClaims c on u.id = c.UserId and claimType = 'Name'
+            order by Name
+            OFFSET (@page - 1 )* @rows ROWS FETCH NEXT @rows ROWS ONLY";
+
+      return db.Query<EmployeeResponse>(query, new { page, rows });
+    }
+}
